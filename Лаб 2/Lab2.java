b05adc6cdf37d497f4f230ac103342211435f2d0
TAPAKAHOKOT <igrok332233@mail.ru>
2021-02-14 23:30:22 +0300

    Added reports for Lab 1 and Lab 2

--- Лаб 2/Lab2.java
+++ Лаб 2/Lab2.java
@@ -9,13 +9,17 @@ public class Lab2{
 		Point3d dot_1 = new Point3d();
 		Point3d dot_2 = new Point3d(1, 2, 3);
 		Point3d dot_3 = new Point3d(1, 1, 1);
+		Point3d dot_4 = new Point3d(20, 1.5, 13);
 
 		System.out.println(computeArea(dot_1, dot_2, dot_3));
+		System.out.println(computeArea(dot_1, dot_2, dot_4));
+		System.out.println(computeArea(dot_1, dot_4, dot_3));
+		System.out.println(computeArea(dot_4, dot_2, dot_3));
 	}
 
 	static double computeArea(Point3d d1, Point3d d2, Point3d d3){
 		if (d1.isEqualDots(d2) || d2.isEqualDots(d3) || d3.isEqualDots(d1)){
-			System.out.println("ERROR: точки совпадают(((")
+			System.out.println("ERROR: точки совпадают(((");
 			return 0d;
 		}
 
